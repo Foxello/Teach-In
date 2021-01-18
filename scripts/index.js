324084d3b19ae50fea99d053eba23aac455638b7
Andrey Bolotov <71553747+Foxello@users.noreply.github.com>
2021-01-18 19:43:26 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
+  // if(localStorage.getItem("ID")){
+  //   document.location.href="../htlm/messanger.html"
+  // }
   localStorage.clear()
-  let socket = new WebSocket("wss://armacoty.tk:1234");
+  let socket = new WebSocket("ws://armacoty.tk:1234");
   const form = document.querySelectorAll("form")
 
   form.forEach(el =>{
@@ -11,38 +14,36 @@ document.addEventListener("DOMContentLoaded",()=>{
   //localStorage.setItem("iwd",41241224)
 
     async function formSend(e){
-    e.preventDefault();
-    let formData = new FormData(e.target);
-    
-    let obj = {};
-    if(e.target.getAttribute('data-request') === 'get_token'){
-      formData.append("request","get_token")
-    }else{
-      formData.append("request","new_token")
-    }
-    let promise = new Promise((resolve,reject) =>{
-      formData.forEach((value,key)=>{
-        obj[key] = value;
+      e.preventDefault();
+      let formData = new FormData(e.target);
+      
+      let obj = {};
+      if(e.target.getAttribute('data-request') === 'get_token'){
+        formData.append("request","get_token")
+      }else{
+        formData.append("request","new_token")
+      }
+      let promise = new Promise((resolve,reject) =>{
+        formData.forEach((value,key)=>{
+          obj[key] = value;
+        })
+        console.log(obj)
+        let json = JSON.stringify(obj);
+        e.target.reset();
+        resolve(json);
       })
-      console.log(obj)
-      let json = JSON.stringify(obj);
-      e.target.reset();
-      resolve(json);
-    })
 
-    promise.then( data =>{
-      console.log(data)
-      socket.send(data)
-      alert("Form is was send")
-    }
-    ).catch(err =>{
-      console.error('Error: ',err)
-    });
+      promise.then( data =>{
+        socket.send(data)
+      }
+      ).catch(err =>{
+        console.error('Error: ',err)
+      });
 
   }
 
   socket.onopen = ()=>{
-    alert("Соединение установлено!");
+    console.log("Соединение установлено!");
   }
 
   socket.onmessage = event =>{
@@ -55,6 +56,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     let userData = JSON.parse(data);
     switch (userData.answer) {
       case "new user successful":
+        alert("Пользователь успешно создан!")
         break;
       case "new user error":
         alert(userData.error);
@@ -64,9 +66,11 @@ document.addEventListener("DOMContentLoaded",()=>{
         localStorage.setItem("name",userData.name)
         localStorage.setItem("last_name",userData.last_name)
         localStorage.setItem("avatar",userData.image)
+        console.log(userData)
+        localStorage.setItem("image",userData.bytes)
         document.location.href = "../html/messanger.html"
+        break;
       case "autorize error":
-        alert(userData.error);
         default:
         break;
     }
--- scripts/messanger.js
+++ scripts/messanger.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     if(!localStorage.getItem("ID")){
         alert("Вы не вошли в аккаунт!")
 
-        document.location.href = "../html/login.html"
+        document.location.href = "../html/login.html";
     }
-    let socket = new WebSocket("wss://armacoty.tk:1234");
+    const id = localStorage.getItem("ID")
+    let socket = new WebSocket("ws://armacoty.tk:1234");
     const userName = localStorage.getItem("name");
     document.querySelector("#headerUserName").innerHTML = userName;
-    document.querySelector(".messanger__headerKid__image").innerHTML = localStorage.getItem("name")[0].toUpperCase() + localStorage.getItem("last_name")[0].toUpperCase();
     const userList = document.querySelector(".messanger_userList__list > ul")
     const messageWindow = document.querySelector(".messanger__massageWindow__messanges > ul")
     const sendButton = document.querySelector("#sendButton")
@@ -19,26 +19,152 @@ document.addEventListener("DOMContentLoaded",()=>{
     const addingList = document.querySelector(".messangerSearchUserList ul")
     let addingListElement;
     let myContactList;
+    let myContactListId;
+    const modalWindow = document.querySelector(".modalWindow")
+    const userImage = document.querySelector(".modalWindow__myProfile__image")
+    let imageReader = new FileReader();
+    const imageFileInput = document.querySelector(".modalWindow__myProfile__image > input")
+    let currentChatId;
+
+    function setUserImage(){
+        console.log("picture is change")
+        document.querySelector(".messanger__headerKid__image").style.background = `url(${localStorage.getItem("image")})`
+    }
+
     findButton.addEventListener("click",()=>{
         let keys = document.querySelector(".messanger_userList__searchUser > input").value.trim().split(" ");
-        let obj = {"search_keys":keys, "request":"find_users"};
-        console.log(obj)
+        let obj = {"search_keys":keys, "request":"find_users","my_id":id};
         socket.send(JSON.stringify(obj))
+    })
+
+    document.querySelectorAll(".profileMenu a:not(#logOutLink)").forEach((el,index) =>{
+        const modalWindows = document.querySelectorAll(".modalWindow >*")
+        el.addEventListener("click",()=>{
+            modalWindow.classList.toggle("windowActive");
+            modalWindows[+!index].classList.add("hidden")
+            modalWindows[index].classList.remove("hidden")
+            document.querySelector(".modalWindow__myProfile__data__userNameBlock span").innerHTML = localStorage.getItem("name")
+            document.querySelector(".modalWindow__myProfile__data__userLastNameBlock span").innerHTML = localStorage.getItem("last_name")
+        })
+    })
+
+    document.querySelectorAll(".modalWindow__exitButton").forEach(el=>{
+        el.addEventListener("click",()=>{
+            modalWindow.classList.toggle("windowActive");
+            el.parentElement.classList.add("hidden")
+        })
+    })
+
+    document.querySelector(".modalWindow__myProfile__data form").addEventListener("submit", sendInfoForm)
+    async function sendInfoForm(e){
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        formData.append("request","change_info")
+        formData.append("my_id",id)
+        let obj = {};
+        let promise = new Promise((resolve,reject) =>{
+            formData.forEach((value,key)=>{
+              obj[key] = value;
+            })
+            let json = JSON.stringify(obj);
+            e.target.reset();
+            resolve(json);
+          })
+    
+          promise.then( data =>{
+            socket.send(data)
+            document.querySelectorAll(".modalWindow__myProfile__data div").forEach(ele=>{
+                resetInfoForm(ele)
+            })
+          }
+          ).catch(err =>{
+            console.error('Error: ',err)
+          });
+
+        }
+
+        function showInfoForm(el,e){
+            el.querySelectorAll("*:not(input)").forEach(ele=>{
+                ele.classList.toggle("hidden")
+            })
+            el.querySelector("input").classList.toggle("hidden")
+        }
+
+        function resetInfoForm(el,e){
+            
+            el.querySelectorAll("*:not(input)").forEach(ele=>{
+                ele.classList.remove("hidden")
+            })
+            el.querySelector("input").classList.add("hidden")
+        }
+
+    imageFileInput.onchange = ()=>{
+        const imageFileInput = document.querySelector(".modalWindow__myProfile__image > input").files[0]
+        // imageReader.onloadend = ()=>{
+        //     userImage.style.background = `url(${imageReader.result})`
+        // }
+        // if(imageFileInput){
+        //     imageReader.readAsDataURL(imageFileInput)
+        // }else{
+
+        // }
+        const img = new Image()
+        img.src =  imageReader.result
+        const width = 150
+        const scaleFactor = width / img.width
+
+            const elem = document.createElement('canvas')
+            elem.width = width
+            elem.height = img.height * scaleFactor
+            const ctx = elem.getContext('2d')
+            ctx.drawImage(img,0,0,width,img.height * scaleFactor)
+            const blobUser = ctx.canvas.toBlob(blob =>{
+                const file = new File([blob], imageFileInput.name, {
+                    type: 'image/jpeg',
+                    lastModified: Date.now()
+                  });
+            },'image/jpeg',1)
+        
+
+        setTimeout(()=>{
+            //let url = imageFileInput.toDataUrl("image/png")
+            localStorage.setItem("image",imageReader.result)
+            socket.send(JSON.stringify({"bytes":imageReader.result, "request":"add_photo", "my_id":id}))
+            console.log("Отправка завершена")
+        },300)
+    }
+
+    // document.querySelector(".modalWindow__myProfile").addEventListener("click",(e)=>{
+    //     document.querySelectorAll(".modalWindow__myProfile__data div").forEach(ele=>{
+    //         resetInfoForm(ele,e)
+    //     })
+    // })
+
+    document.querySelectorAll(".modalWindow__myProfile__dataBlock").forEach(el=>{
+        el.addEventListener("click",(e)=>{
+            showInfoForm(el,e)
+        })
     })
 
     document.querySelector("#logOutLink").addEventListener("click",()=>{
         localStorage.clear();
-        socket.send(JSON.stringify({"request":"log_out"}));
+        socket.send(JSON.stringify({"request":"log_out","my_id":id}));
         document.location.href = "../html/login.html";
     });
 
     sendButton.addEventListener("click",()=>{
-        socket.send(JSON.stringify({"request":"add_message","message":messangerInput.value}))
+        if(!messangerInput.value){
+            return;
+        }
+        socket.send(JSON.stringify({"request":"add_message","message":messangerInput.value,"my_id":id,"id":currentChatId}))
         createMessage({"from":"","message":messangerInput.value})
     })
     document.addEventListener("keydown",(e)=>{
         if(e.which === 13){
-            socket.send(JSON.stringify({"request":"add_message","message":messangerInput.value}))
+            if(!messangerInput.value){
+                return;
+            }
+            socket.send(JSON.stringify({"request":"add_message","message":messangerInput.value,"my_id":id,"id":currentChatId}))
             createMessage({"from":"","message":messangerInput.value})
         }
         if(e.which === 27 && !addingList.parentElement.classList.contains("hidden")){
@@ -52,21 +178,19 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
 
     function createMessage(el){
-        // if(!messangerInput.value){
-        //     return;
-        // }
         let name;
         let div = document.createElement('li');
         if(el.from){
             div.classList.add("other")
             name = el.from
         }else{
-            div.classList.add("seft")
+            div.classList.add("self")
             name = localStorage.getItem("name");
         }
         div.innerHTML = `<div><span>${name}</span><p>${el.message}</p></div>`;
         messageWindow.appendChild(div);
         messangerInput.value = "";
+        messageWindow.parentElement.scrollTop = messageWindow.parentElement.scrollHeight - messageWindow.parentElement.clientHeight
     }
 
     
@@ -83,15 +207,15 @@ document.addEventListener("DOMContentLoaded",()=>{
             li.setAttribute("data-id",el.id)
             addingList.appendChild(li)
         })
-        listItemEvent()
+        listSearchItemEvent()
     }
 
     refreshButton.addEventListener("click",(e)=>{
         e.target.classList.toggle("active")
-        socket.send(JSON.stringify({"request":"all_chats"}));
+        socket.send(JSON.stringify({"request":"all_chats","my_id":id}));
         let onlineUsers= setInterval(()=>{
-            socket.send(JSON.stringify({"request":"online"}))
-        },60000)
+            socket.send(JSON.stringify({"request":"online","my_id":id}))
+        },30000)
     })
 
     function createAllMessages(array){
@@ -99,33 +223,65 @@ document.addEventListener("DOMContentLoaded",()=>{
         array.forEach(el =>{
             createMessage(el)
         })
+        messageWindow.parentElement.scrollTop = messageWindow.parentElement.scrollHeight - messageWindow.parentElement.clientHeight
     }
 
     function createContactList(array){
         myContactList = array.map(el=> el)
+        myContactListId = array.map(el => el.id)
         refreshButton.classList.add("hidden")
         array.forEach(el =>{
             let li = document.createElement("li")
-            li.classList.add("listItem", "offline")
-            li.innerHTML = `<span>${el.name} ${el.last_name}</span>`
+            li.classList.add("listItem", el.status)
+            li.innerHTML = `<span>${el.name} ${el.last_name}</span><div class="countMes hidden"></div>`
             li.setAttribute("data-id",el.id)
             userList.appendChild(li)
         })
         listItemEvent()
+        
+    }
+
+    function listSearchItemEvent(){
+        addingListElement = document.querySelectorAll(".messangerSearchUserList .listItem")
+        addingListElement.forEach(el =>{
+            el.addEventListener("click",()=>{
+                document.querySelector(".messanger__messageWindow").classList.remove("noneChat")
+                currentChatId = el.getAttribute("data-id")
+                addingList.parentElement.classList.toggle("hidden")
+                addingList.innerHTML = "";
+                let li = document.createElement("li")
+                li.classList.add("listItem", "offline", "currentListItem")
+                li.innerHTML = `<span>${el.lastChild.innerHTML}</span><div class="countMes hidden"></div>`
+                li.setAttribute("data-id",el.id)
+                userList.appendChild(li)
+                document.querySelector(".messanger_userList__searchUser > input").value = ""
+                listItemEvent()
+                socket.send(JSON.stringify({"request":"open_chat","id":el.getAttribute("data-id"),"my_id":id}))
+            })
+        })
     }
 
     function listItemEvent(){
-        addingListElement = document.querySelectorAll(".listItem")
+        addingListElement = document.querySelectorAll(".messanger_userList__list > ul .listItem")
         addingListElement.forEach(el =>{
             el.addEventListener("click",()=>{
                 addingListElement.forEach(e =>{
                     e.classList.remove("currentListItem")
                 })
+                document.querySelector(".messanger__messageWindow").classList.remove("noneChat")
+                document.querySelector(".messanger__messageWindow").style.zIndex = 21
+                el.querySelector(".countMes").classList.add("hidden")
+                el.querySelector(".countMes").setAttribute("data-countM",0)
+                currentChatId = el.getAttribute("data-id")
                 el.classList.add("currentListItem")
-                socket.send(JSON.stringify({"request":"open_chat","id":el.getAttribute("data-id")}))
+                socket.send(JSON.stringify({"request":"open_chat","id":el.getAttribute("data-id"),"my_id":id}))
              }) 
         })
     }
+
+    document.querySelector(".messangerHeader img").addEventListener("click",()=>{
+        document.querySelector(".messanger__messageWindow").style.zIndex = 19
+    })
 
     function updateContactsState(array){
         array.forEach(el =>{
@@ -138,14 +294,61 @@ document.addEventListener("DOMContentLoaded",()=>{
         })
     }
 
+    function updateMessage(data){
+        if(currentChatId == data.id){
+            createMessage(data)
+        }
+        else{
+            
+            if(
+                myContactListId.indexOf(data.id) != -1
+            ){
+                let ci = document.querySelector(`.messanger_userList__list>ul>li[data-id='${data.id}'] div.countMes`)
+                ci.classList.remove("hidden")
+                if(ci.getAttribute("data-count")){
+                    ci.parentElement.setAttribute("data-count",ci.getAttribute("data-count")++)
+                }else{
+                    ci.parentElement.setAttribute("data-count",1)
+                }
+                ci.innerHTML = ci.parentElement.getAttribute("data-count")
+                const audio = new Audio()
+                audio.src = "../images/sound.mp3"
+                audio.play()
+            }else{
+                let ci = document.createElement("li")
+                ci.classList.add("listItem", "offline")
+                ci.innerHTML = `<span>${data.from}</span><div class="countMes hidden"></div>`
+                ci.setAttribute("data-id",data.id)
+                userList.appendChild(ci)
+                myContactListId.push(data.id)
+                listItemEvent()
+                let ciI = document.querySelector(`.messanger_userList__list>ul>li[data-id='${data.id}'] div.countMes`)
+                ciI.classList.remove("hidden")
+                if(ci.getAttribute("data-count")){
+                    ciI.parentElement.setAttribute("data-count",ci.getAttribute("data-count")++)
+                }else{
+                    ciI.parentElement.setAttribute("data-count",1)
+                }
+                ciI.innerHTML = ciI.parentElement.getAttribute("data-count")
+                const audio = new Audio()
+                audio.src = "../images/sound.mp3"
+                audio.play()
+            }
+
+            
+            
+        }
+
+    }
+
     //websocket
     socket.onopen = ()=>{
-        alert("Соединение установлено!");
+        console.log("Соединение установлено!");
+        //setUserImage();
         socket.send(JSON.stringify({"request":"confirm_token","id":localStorage.getItem("ID"),"name":localStorage.getItem("name")}))
     }
     
     socket.onmessage = event =>{
-        console.log(event.data);
         formHandler(event.data)
     }
 
@@ -153,25 +356,23 @@ document.addEventListener("DOMContentLoaded",()=>{
         let userData = JSON.parse(data);
         switch (userData.answer) {
           case "all_find_client":
-              console.log(userData.client)
               createNewContact(userData.client);
               break;
           case "successful confirm":
-              console.log(userData.answer)
               break;
           case "all_chats successful":
-              console.log(userData.users)
+              console.log(data)
               createContactList(userData.users);
               break;
           case "chats_with":
-              console.log(userData.message)
               createAllMessages(userData.message)
               break;
           case "online":
-              console.log(userData.users)
               updateContactsState(userData.users)
               break;
-          
+          case "new_message":
+              updateMessage(userData)
+              break;
           default:
               break;
             
